f885c93c5da9eb6c77e15e6c560d7f6ea9a276f9
lalitkumar100 <166192372+lalitkumar100@users.noreply.github.com>
2025-03-28 05:26:38 +0530

    Create pivotIndex(.java

---
+++ pivotIndex(.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int totalSum = 0;
+        for (int num : nums) {
+            totalSum += num;
+        }
+
+        int leftSum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (leftSum == totalSum - leftSum - nums[i]) {
+                return i; // Found pivot index
+            }
+            leftSum += nums[i]; // Update left sum
+        }
+
+        return -1; // No pivot index found
+    }
+}
