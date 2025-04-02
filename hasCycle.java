e66d63e3d157599e170c4d41c806b49cb2fbe348
lalitkumar100 <166192372+lalitkumar100@users.noreply.github.com>
2025-04-02 06:42:23 +0530

    Create hasCycle.java

---
+++ hasCycle.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+     
+     ListNode slow =head,fast =head;
+
+        while(fast.next!=null&&fast!=null){
+            fast =fast.next;
+            if(fast==null){
+                return false;
+            }
+       fast =fast.next;
+         
+            slow =slow.next;
+           if(fast==slow){
+            return true;
+           }
+        }
+        return false;
+    }
+}
